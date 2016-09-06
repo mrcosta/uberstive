16d0a8efa1d3a2c1a1b86254a6142d4ae8a897d9
anapaulagomes <apgomes88@gmail.com>
2016-09-06 00:27:53 -0300

    Remove sensitive data, comments and fix the port number

--- uberstive/mod_data_retrieving/uber_data.py
+++ uberstive/mod_data_retrieving/uber_data.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-
 from flask import render_template, redirect, request
 from uberstive import app
 from rauth import OAuth2Service
 import requests
+import os
+
+UBER_API_URL = 'https://sandbox-api.uber.com/v1/'
 
 
 @app.route('/', methods=['GET'])
@@ -13,7 +14,6 @@ def history():
 def user_history():
     # https://api.uber.com/v1.2/history
     # parameters = {'apikey': PUBLIC_KEY, 'ts': timestamp, 'hash': hash_, 'limit': 100, 'offset': n}
-
     # history = requests.get('http://gateway.marvel.com/v1/public/characters', params=parametros)  # requisica
     return "ana delicia"
 
@@ -21,29 +21,29 @@ def user_history():
 def login():
 
   uber_api = OAuth2Service(
-    client_id='EsBJfHieCNX8wIGVrYCjvB0o-iIZf78Y',
-    client_secret='WPrvB3CRxggnHXWqzdGKPbyohJCpkyj7epBAp7FU',
+    client_id=os.environ['CLIENT_ID'],
+    client_secret=os.environ['CLIENT_SECRET'],
     name='stive',
     authorize_url='https://login.uber.com/oauth/authorize',
     access_token_url='https://login.uber.com/oauth/token',
-    base_url='https://sandbox-api.uber.com/v1/'
+    base_url=UBER_API_URL,
   )
 
   parameters = {
     'response_type': 'code',
-    'redirect_uri': 'http://localhost:5000/oauth/cb', # your callback url for oauth2 step 2
+    'redirect_uri': 'http://localhost:5000/oauth/cb',
     'scope': 'profile',
   }
 
-  # Redirect user here to authorize your application
   login_url = uber_api.get_authorize_url(**parameters)
   return redirect(login_url)
+
 
 @app.route('/oauth/cb')
 def test():
 
   parameters = {
-    'redirect_uri': 'http://localhost:8080/oauth/cb',
+    'redirect_uri': 'http://localhost:5000/oauth/cb',
     'code': request.args.get('code'),
     'grant_type': 'authorization_code',
   }
@@ -51,40 +51,21 @@ def test():
   response = requests.post(
     'https://login.uber.com/oauth/token',
     auth=(
-      'EsBJfHieCNX8wIGVrYCjvB0o-iIZf78Y',
-      'WPrvB3CRxggnHXWqzdGKPbyohJCpkyj7epBAp7FU',
+      os.environ['CLIENT_ID'],
+      os.environ['CLIENT_SECRET'],
     ),
     data=parameters,
   )
 
-
-  # This access_token is what we'll use to make requests in the following steps
   access_token = response.json().get('access_token')
-  print(response)
-
-  return "mateus"
-  # access_token = request.args.get('access_token')
-
-  #response = requests.get(
-  #  'https://api.uber.com/v1' + '/me',
-  #  headers={
-  #    'Authorization': 'Bearer %s' % access_token
-  #  }
-  #)
-
-  #data = response.text
-  #return data
-
-
-  # return access_token
+  return access_token
 
 @app.route('/profile')
 def profile():
-
   access_token = request.args.get('access_token')
 
   response = requests.get(
-    'https://api.uber.com/v1' + '/me',
+    UBER_API_URL + 'me',
     headers={
         'Authorization': 'Bearer %s' % access_token
     }
